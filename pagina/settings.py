9d65d3e7e05c5fb5ad4f83ae0047f1e0bcf30cf1
Haroldhv4 <152220718+Haroldhv4@users.noreply.github.com>
2024-01-04 09:15:56 -0500

    cambios

--- pagina/settings.py
+++ pagina/settings.py
@@ -21,14 +21,14 @@ import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.enviroment.get('SECRET_KEY', default='sdfs66fs6d6f6ds6f6')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='sdfs66fs6d6f6ds6f6')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os environ
 
 ALLOWED_HOSTS = []
 
-RENDER_EXTERNAL_HOSTNAME=os.enviroment.get('RENDER_EXTERNAL_HOSTNAME')
+RENDER_EXTERNAL_HOSTNAME=os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
